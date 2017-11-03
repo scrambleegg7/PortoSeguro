6d4a80837f6e93a55f9f3f8205141eb8f023be97
hideaki <donchan@milano.local>
2017-11-03 19:10:39 +0900

    Add model3 / Feat programs  ....

--- Feat/znull.py
+++ Feat/znull.py
@@ -6,6 +6,7 @@ from replace_nan import replace_nan
 
 def zNullCount(df):
     df = replace_nan(df)
+    print("zero null count.....")
     numberOfNullCols7 = df.isnull().sum(axis=1)
     df = df.assign(znull=numberOfNullCols7)
     df.fillna(-1,inplace=True)
