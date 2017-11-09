2da295e1ac9202f28f756c4a647d0f175e9d1df6
hideaki linux hp <rmhmn777@yahoo.co.jp>
2017-11-09 20:57:31 +0900

    keras model initial commit

---
+++ keras/callBClass.py
@@ -0,0 +1,47 @@
+#
+#
+
+import numpy as np
+#np.random.seed()
+import pandas as pd
+from datetime import datetime
+from sklearn.metrics import log_loss, roc_auc_score
+
+
+from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger, Callback
+
+class roc_auc_callback(Callback):
+    def __init__(self,training_data,validation_data):
+        self.x = training_data[0]
+        self.y = training_data[1]
+        self.x_val = validation_data[0]
+        self.y_val = validation_data[1]
+
+    def on_train_begin(self, logs={}):
+        return
+
+    def on_train_end(self, logs={}):
+        return
+
+    def on_epoch_begin(self, epoch, logs={}):
+        return
+
+    def on_epoch_end(self, epoch, logs={}):
+        y_pred = self.model.predict_proba(self.x, verbose=0)
+        roc = roc_auc_score(self.y, y_pred)
+        logs['roc_auc'] = roc_auc_score(self.y, y_pred)
+        logs['norm_gini'] = ( roc_auc_score(self.y, y_pred) * 2 ) - 1
+
+        y_pred_val = self.model.predict_proba(self.x_val, verbose=0)
+        roc_val = roc_auc_score(self.y_val, y_pred_val)
+        logs['roc_auc_val'] = roc_auc_score(self.y_val, y_pred_val)
+        logs['norm_gini_val'] = ( roc_auc_score(self.y_val, y_pred_val) * 2 ) - 1
+
+        print('\rroc_auc: %s - roc_auc_val: %s - norm_gini: %s - norm_gini_val: %s' % (str(round(roc,5)),str(round(roc_val,5)),str(round((roc*2-1),5)),str(round((roc_val*2-1),5))), end=10*' '+'\n')
+        return
+
+    def on_batch_begin(self, batch, logs={}):
+        return
+
+    def on_batch_end(self, batch, logs={}):
+        return
